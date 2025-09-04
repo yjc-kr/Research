5439366e629d816577dcdaa5f547304cb2219bc7
yjc-kr <79780820+yjc-kr@users.noreply.github.com>
2025-09-04 18:05:14 +0900

    20250904_1805

--- Polytomy/Codes/r/Generate Tree (poweRbal).r
+++ Polytomy/Codes/r/Generate Tree (poweRbal).r
@@ -3,6 +3,8 @@ library("ape")
 
 num <- 1024
 n_param <- 16
+
+# AldousBetaTree_n16_b1
 BETA_param <- 1
 
 AldousBetaTrees_nwk <- character(num)
@@ -12,5 +14,41 @@ for(i in 1:num){
     AldousBetaTrees_nwk[i] <- write.tree(tree_phy)
 }
 
-head(AldousBetaTrees_nwk, 3)
-writeLines(AldousBetaTrees_nwk, "AldousBetaTrees_n16_b1.nwk")
+writeLines(AldousBetaTrees_nwk, "Polytomy/Codes/nwk/AldousBetaTrees_n16_b1.nwk")
+
+# AldousBetaTree_n16_b0 (Yule)
+BETA_param <- 0
+
+AldousBetaTrees_nwk <- character(num)
+
+for(i in 1:num){
+    tree_phy <- genAldousBetaTree(n = n_param, BETA = BETA_param)
+    AldousBetaTrees_nwk[i] <- write.tree(tree_phy)
+}
+
+writeLines(AldousBetaTrees_nwk, "Polytomy/Codes/nwk/AldousBetaTrees_n16_b0.nwk")
+
+# AldousBetaTree_n16_b-1.5 (PDA model)
+BETA_param <- -1.5
+
+AldousBetaTrees_nwk <- character(num)
+
+for(i in 1:num){
+    tree_phy <- genAldousBetaTree(n = n_param, BETA = BETA_param)
+    AldousBetaTrees_nwk[i] <- write.tree(tree_phy)
+}
+
+writeLines(AldousBetaTrees_nwk, "Polytomy/Codes/nwk/AldousBetaTrees_n16_b-1.5.nwk")
+
+# AldousBetaTree_n16_b-2 (Comb Tree)
+BETA_param <- -2
+
+AldousBetaTrees_nwk <- character(num)
+
+for(i in 1:num){
+    tree_phy <- genAldousBetaTree(n = n_param, BETA = BETA_param)
+    AldousBetaTrees_nwk[i] <- write.tree(tree_phy)
+}
+
+writeLines(AldousBetaTrees_nwk, "Polytomy/Codes/nwk/AldousBetaTrees_n16_b-2.nwk")
+
